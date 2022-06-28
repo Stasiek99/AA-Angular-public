c816ce5217a7a4790091fc8dac65e192709adbfa
Stasiek99 <stasiekpilich@gmail.com>
2022-06-28 13:13:14 +0200

    Added feature module

--- src/app/country-search-engine/country-search-engine.module.ts
+++ src/app/country-search-engine/country-search-engine.module.ts
@@ -3,11 +3,11 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 
-import { CountrySearchEngineComponent } from './country-search-engine.component';
-import {SearchInputComponent} from "../components/search-input/search-input.component";
-import {SearchAutocompleteComponent} from "../components/search-autocomplete/search-autocomplete.component";
-import {CountryDataService} from "../services/country-data.service";
-import {AppComponent} from "../../app.component";
+import { CountrySearchEngineComponent } from './country-search-engine/country-search-engine.component';
+import {SearchInputComponent} from "./components/search-input/search-input.component";
+import {SearchAutocompleteComponent} from "./components/search-autocomplete/search-autocomplete.component";
+import {CountryDataService} from "./services/country-data.service";
+import {AppComponent} from "../app.component";
 
 @NgModule({
   declarations: [
