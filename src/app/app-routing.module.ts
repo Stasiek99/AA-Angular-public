d7c8df40657dbe6ccdcfa88728a81095a5bdaa31
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2022-06-30 19:29:12 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
-import {Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
 import {HomeComponent} from "./home/components/home.component";
 import {CountrySearchEngineComponent} from "./country-search-engine/country-search-engine/country-search-engine.component";
+import {PageNotFoundComponent} from "./general/components/page-not-found/page-not-found.component";
 
 export const routeConfig: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-  },
-  {
-    path: "search-bar",
-    component: CountrySearchEngineComponent
-  }
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "search-bar", component: CountrySearchEngineComponent},
+  {path: "**", pathMatch: "full", component: PageNotFoundComponent}
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routeConfig)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule{}
