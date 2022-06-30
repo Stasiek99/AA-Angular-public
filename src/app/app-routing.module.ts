f0a3881c7e0e2dce94d48d6656ff73308ade96b3
Stasiek99 <stasiekpilich@gmail.com>
2022-06-30 12:55:24 +0200

    Routing 2/2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {HomeComponent} from "./home/components/home.component";
 import {CountrySearchEngineComponent} from "./country-search-engine/country-search-engine/country-search-engine.component";
+import {PageNotFoundComponent} from "./general/components/page-not-found/page-not-found.component";
 
 export const routeConfig: Routes = [
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
-  {path: "search-bar", component: CountrySearchEngineComponent}
+  {path: "search-bar", component: CountrySearchEngineComponent},
+  {path: "**", pathMatch: "full", component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   exports: [
     HeaderComponent,
