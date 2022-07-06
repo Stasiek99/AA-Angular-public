878cf0ad360d60fdd2f0d7b307d3e7a60aa091dd
Stasiek99 <stasiekpilich@gmail.com>
2022-07-06 17:57:58 +0200

    Angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {CountryDataService} from "./country-search-engine/services/country-data.service";
@@ -14,7 +15,7 @@ import {CreateNewUserModule} from "./create-new-user/create-new-user.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +24,9 @@ import {CreateNewUserModule} from "./create-new-user/create-new-user.module";
     GeneralModule,
     HomeModule,
     AppRoutingModule,
-    CreateNewUserModule
+    CreateNewUserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [HttpClientModule, CountryDataService],
   bootstrap: [AppComponent]
