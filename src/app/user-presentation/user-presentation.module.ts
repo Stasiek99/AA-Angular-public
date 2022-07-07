93767259a0ca715dfd0b75be965b4dfdc8465501
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2022-07-07 20:20:15 +0200

    User service (#13)

--- src/app/user-presentation/user-presentation.module.ts
+++ src/app/user-presentation/user-presentation.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 
 import {UserPresentationComponent} from "./components/user-presentation.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [UserPresentationComponent],
+  imports: [CommonModule],
   exports: [UserPresentationComponent]
 })
 export class UserPresentationModule {}
