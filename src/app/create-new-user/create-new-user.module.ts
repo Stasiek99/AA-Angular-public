eff1e95c2d9bd3e647291be91c3af263cf087cdd
Stasiek99 <stasiekpilich@gmail.com>
2022-07-08 10:22:36 +0200

    Added flex layout in CreateNewUserComponent

--- src/app/create-new-user/create-new-user.module.ts
+++ src/app/create-new-user/create-new-user.module.ts
@@ -1,11 +1,22 @@
 import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
 
 import {CreateNewUserComponent} from "./components/create-new-user.component";
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [CreateNewUserComponent],
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    FlexLayoutModule,
+    FlexModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   exports: [CreateNewUserComponent]
 })
 export class CreateNewUserModule{}
