e12808265a0c06d0764857733dc8684a686680cf
Stasiek99 <stasiekpilich@gmail.com>
2022-07-31 16:30:23 +0200

    Dialog refactored

--- src/app/create-new-user/components/create-new-user/create-new-user.component.ts
+++ src/app/create-new-user/components/create-new-user/create-new-user.component.ts
@@ -13,21 +13,18 @@ import {UserStateService} from "../../../user/user-state.service";
 })
 export class CreateNewUserComponent {
   @ViewChild("f") userDataForm!: NgForm;
-  passedValues: any;
 
   constructor(private dialog: MatDialog, private router: Router, private userService: UserStateService) {}
 
   openDialog(): void {
    let dialogRef = this.dialog.open(DialogCreateUserComponent, {
-      data: this.userDataForm,
       height: "200px",
       width: "400px"
     });
    dialogRef.afterClosed().subscribe(result => {
        if(result){
-           this.passedValues = result.value;
-           this.userService.setUser(this.passedValues);
-           this.redirectToUserPresentation();
+         this.userService.setUser(this.userDataForm.value);
+         this.redirectToUserPresentation();
        }
    });
   }
